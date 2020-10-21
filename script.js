f7d41de851821fe52f0a9315ce33f676a240bd62
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-10-20 20:39:31 -0400

    defining variables

---
+++ script.js
@@ -0,0 +1,7 @@
+// Storing the value of the search term as a variable
+var city = $("#search-bar").val();
+
+// Storing the openweather api key as a variable
+var api = "&appid=84d50c53f3586240f1958d7f0775cb40";
+
+var date = new Date();
