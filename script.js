e339f3064ebd6b7fa712e34106c8e2bbfb8457cc
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-10-20 21:00:54 -0400

    search bar event handler, querying the api for current weather

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ var city = $("#search-bar").val();
 var api = "&appid=84d50c53f3586240f1958d7f0775cb40";
 
 var date = new Date();
+
+$("#search-bar").on("click", function () {
+  // Clear the search bar
+  $("search-bar").val("");
+  // Show the forecast div
+  $("#forecast").addClass("show");
+  city = $("search-bar").val();
+
+  // Using ajax to make api call for current weather query
+  $.ajax({
+    url: "api.openweathermap.org/data/2.5/weather?q" + city + api,
+    method: "GET",
+  }).then(function (response) {
+    // Converting the temp from the api call to fahrenheit
+    var temperature = (response.main.temp - 273.15) * 1.8 + 32;
+    console.log(Math.floor(temperature));
+    temperature = Math.floor(temperature);
+  });
+});
